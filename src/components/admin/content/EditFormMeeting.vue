df320fcd2dede37ddac3b3735f1d4a27ce732406
Ychen <848112638@qq.com>
2024-01-17 21:42:29 +0800

    修复bug

--- src/components/admin/content/EditFormMeeting.vue
+++ src/components/admin/content/EditFormMeeting.vue
@@ -2,45 +2,59 @@
   <div style="text-align: left">
     <el-button class="add-button" type="success" @click="dialogFormVisible = true">添加会议</el-button>
     <el-dialog
-            title="修改活动信息"
-            :visible.sync="dialogFormVisible"
-            @close="clear">
-      <el-form :model="form" :rules="rules"   style="text-align: left" ref="form">
+        title="修改活动信息"
+        :visible.sync="dialogFormVisible"
+        @close="clear">
+      <el-form :model="form" :rules="rules" style="text-align: left" ref="form">
         <el-form-item label="活动名称" :label-width="formLabelWidth" prop="meetingName">
           <el-input v-model="form.meetingName" autocomplete="off" placeholder="活动名称"></el-input>
         </el-form-item>
-        <el-form-item label="房间名" :label-width="formLabelWidth" prop="roomName">
-          <el-input v-model="form.roomName" autocomplete="off"></el-input>
+
+
+        <!--        <el-form-item label="教室" :label-width="formLabelWidth" prop="roomName">-->
+        <!--          <el-input v-model="form.roomName" autocomplete="off"></el-input>-->
+        <!--        </el-form-item>-->
+        <el-form-item label="教室名" :label-width="formLabelWidth">
+          <el-select v-model="roomId" placeholder="请选择" @change="getRoomInfo($event)">
+            <el-option
+                v-for="item in rooms"
+                :key="item.roomId"
+                :label="item.roomName"
+                :value="item.roomId">
+            </el-option>
+          </el-select>
         </el-form-item>
+
         <el-form-item label="描述" :label-width="formLabelWidth" prop="description">
           <el-input v-model="form.description" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="状态" :label-width="formLabelWidth" prop="status">
           <el-radio-group v-model="form.status" disabled>
-          <el-radio :label='0' autocomplete="off">已取消</el-radio>
-          <el-radio :label='1' autocomplete="off">已预定</el-radio>
-          <el-radio  :label='2' autocomplete="off">正在进行</el-radio>
-            <el-radio  :label='-1' autocomplete="off">已经过期</el-radio>
+            <el-radio :label='0' autocomplete="off">已取消</el-radio>
+            <el-radio :label='1' autocomplete="off">已预定</el-radio>
+            <el-radio :label='2' autocomplete="off">正在进行</el-radio>
+            <el-radio :label='-1' autocomplete="off">已经过期</el-radio>
           </el-radio-group>
         </el-form-item>
 
         <el-form-item label="使用时间" :label-width="formLabelWidth">
           <el-col :span="11">
             <el-form-item prop="startTime">
-              <el-date-picker type="datetime" placeholder="选择日期时间" v-model="form.startTime" 
-                              style="width: 100%;" ></el-date-picker>
+              <el-date-picker type="datetime" placeholder="选择日期时间" v-model="form.startTime"
+                              style="width: 100%;"></el-date-picker>
             </el-form-item>
           </el-col>
           <el-col class="line" :span="2" style="text-align: center">-</el-col>
           <el-col :span="11">
             <el-form-item prop="endTime">
-              <el-date-picker type="datetime" placeholder="选择日期时间" v-model="form.endTime" 
+              <el-date-picker type="datetime" placeholder="选择日期时间" v-model="form.endTime"
                               style="width: 100%;"></el-date-picker>
             </el-form-item>
           </el-col>
         </el-form-item>
         <el-form-item label="使用人数" :label-width="formLabelWidth" prop="numberOfParticipants">
-          <el-input type="number" v-model="form.numberOfParticipants" autocomplete="off" placeholder="正整数"></el-input>
+          <el-input type="number" v-model="form.numberOfParticipants" autocomplete="off"
+                    placeholder="正整数"></el-input>
         </el-form-item>
         <el-form-item label="预定人" :label-width="formLabelWidth" prop="empName">
           <el-input v-model="form.empName" autocomplete="off" placeholder="预订人"></el-input>
@@ -58,130 +72,159 @@
 </template>
 
 <script>
-  import moment from "moment";
+import moment from "moment";
 
-  export default {
-    name: 'EditForm',
-    data () {
-      const checkStartTime = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error("时间不能为空"));
+export default {
+  name: 'EditForm',
+  data() {
+    const checkStartTime = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("时间不能为空"));
+      }
+      setTimeout(() => {
+        if (moment(value, "YYYY-MM-DD HH:mm:ss").isBefore(moment())) {
+          return callback(new Error("开始时间不能早于现在"));
+        } else {
+          callback();
         }
-        setTimeout(() => {
-          if (moment(value,"YYYY-MM-DD HH:mm:ss").isBefore(moment())) {
-            return callback(new Error("开始时间不能早于现在"));
+      }, 1000);
+
+    };
+    const checkEndTimeToStart = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("时间不能为空"));
+      }
+      setTimeout(() => {
+        if (moment(value, "YYYY-MM-DD HH:mm:ss").isBefore(moment(this.form.startTime, "YYYY-MM-DD HH:mm:ss"))) {
+          return callback(new Error("结束时间不能早于开始时间"));
+        } else {
+          console.log(moment(value, "YYYY-MM-DD HH:mm:ss").diff(moment(this.form.startTime, "YYYY-MM-DD HH:mm:ss")))
+          if (moment(value, "YYYY-MM-DD HH:mm:ss").diff(moment(this.form.startTime, "YYYY-MM-DD HH:mm:ss"), "hours") > 6) {
+            return callback(new Error("不能预定超过6个小时的活动"));
           } else {
             callback();
           }
-        }, 1000);
-
-      };
-      const checkEndTimeToStart = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error("时间不能为空"));
         }
-        setTimeout(() => {
-          if (moment(value,"YYYY-MM-DD HH:mm:ss").isBefore(moment(this.form.startTime,"YYYY-MM-DD HH:mm:ss"))) {
-            return callback(new Error("结束时间不能早于开始时间"));
-          } else {
-            console.log(moment(value,"YYYY-MM-DD HH:mm:ss").diff(moment(this.form.startTime,"YYYY-MM-DD HH:mm:ss")))
-            if (moment(value,"YYYY-MM-DD HH:mm:ss").diff(moment(this.form.startTime,"YYYY-MM-DD HH:mm:ss"), "hours") > 6) {
-              return callback(new Error("不能预定超过6个小时的活动"));
-            }else {
-              callback();
-            }
-          }
-        }, 1000);
-      };
+      }, 1000);
+    };
 
-      const checkNumberOfParticipants = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('人数不能为空'));
+    const checkNumberOfParticipants = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('人数不能为空'));
+      }
+      setTimeout(() => {
+        if (value <= 0) {
+          callback(new Error('不为0或者负数的值'));
+        } else {
+          callback();
         }
-        setTimeout(() => {
-          if (value<=0) {
-            callback(new Error('不为0或者负数的值'));
-          } else {
-            callback();
-          }
-        }, 1000);
-      };
+      }, 1000);
+    };
 
-      return {
-        rules: {
-          meetingName: [
-            {required: true, message: '请输入活动名称', trigger: 'blur'},
-            {min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur'}
-          ],
-          startTime: [
-            {validator:checkStartTime,type: 'date', trigger: 'change'}
-          ],
-          endTime: [
-            {validator:checkEndTimeToStart,type: 'date', trigger: 'change'}
-          ],
-          numberOfParticipants: [
-            {validator: checkNumberOfParticipants, trigger: 'change'}
-          ],
-          description: [
-            {required: true, message: '请填写活动形式', trigger: 'blur'}
-          ]
-        },
-        dialogFormVisible: false,
-        form: {
+    return {
+      rules: {
+        meetingName: [
+          {required: true, message: '请输入活动名称', trigger: 'blur'},
+          {min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur'}
+        ],
+        startTime: [
+          {validator: checkStartTime, type: 'date', trigger: 'change'}
+        ],
+        endTime: [
+          {validator: checkEndTimeToStart, type: 'date', trigger: 'change'}
+        ],
+        numberOfParticipants: [
+          {validator: checkNumberOfParticipants, trigger: 'change'}
+        ],
+        description: [
+          {required: true, message: '请填写活动形式', trigger: 'blur'}
+        ]
+      },
+      dialogFormVisible: false,
+      form: {},
+      rooms: [],
+      roomId: '',
+      roomName: "",
+      reservationTime: '',
+      formLabelWidth: '120px'
+    }
+  },
+  methods: {
+    clear() {
+      this.form = {}
+    },
+    getRooms() {
+      this.$axios.get("/rooms").then(res => {
+        this.rooms = res.data.result;
+      })
+    },
 
-        },
-        formLabelWidth: '120px'
+    getRoomInfo(val) {
+      for (let i = 0; i < this.rooms.length; i++) {
+        const room = this.rooms[i];
+        // Check if roomId is equal to val
+        if (room.roomId === val) {
+          // Assign roomName to data's roomName
+          this.roomName = room.roomName;
+          break; // Break the loop once a match is found
+        }
       }
     },
-    methods: {
-      clear () {
-        this.form = {}
-      },
-      onSubmit (formName) {
-        // this.form.startTime=moment(this).format("YYYY-MM-DD'T'HH:mm:ss")
-        // this.form.endTime=moment(this).format("YYYY-MM-DD'T'HH:mm:ss")
-        // this.form.canceledTime=moment(this).format("YYYY-MM-DD'T'HH:mm:ss")
-        // this.form.reservationTime=moment(this).format("YYYY-MM-DD'T'HH:mm:ss")
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            console.log(this.form)
-            this.$axios
-                    .post('/meeting/save', {
-                      meetingId: this.form.meetingId,
-                      meetingName: this.form.meetingName,
-                      reservationIsTid: this.form.reservationIsTid,
-                      numberOfParticipants: this.form.numberOfParticipants,
-                      startTime: this.form.startTime,
-                      endTime: this.form.endTime,
-                      reservationTime:this.form.reservationTime,
-                      canceledTime: this.form.canceledTime,
-                      description: this.form.description,
-                      status: this.form.status,
-                      roodId: this.form.roodId,
-                      roomName: this.form.roomName,
-                      empName: this.form.empName,
-                      canceledReason: this.form.canceledReason,
+    onSubmit(formName) {
+      const currentDate = new Date();
 
-                    }).then(resp => {
-              if (resp && resp.status === 200) {
-                this.dialogFormVisible = false;
-                this.$emit('onSubmit')
-              }
-            })
-          }else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      }
+      const year = currentDate.getFullYear();
+      const month = String(currentDate.getMonth() + 1).padStart(2, '0');  // 使用 padStart 补齐为两位数
+      const day = String(currentDate.getDate()).padStart(2, '0');
+      const hours = String(currentDate.getHours()).padStart(2, '0');
+      const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+      const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
+      const formattedTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+      this.reservationTime = formattedTime;
+
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$axios
+              .post('/meeting/save', {
+                meetingId: this.form.meetingId,
+                meetingName: this.form.meetingName,
+                reservationIsTid: this.form.reservationIsTid,
+                numberOfParticipants: this.form.numberOfParticipants,
+                startTime: this.form.startTime,
+                endTime: this.form.endTime,
+                reservationTime: this.reservationTime,
+                canceledTime: this.form.canceledTime,
+                description: this.form.description,
+                status: "1",
+                roomId: this.roomId,
+                roomName: this.roomName,
+                empName: this.form.empName,
+                canceledReason: this.form.canceledReason,
+
+              }).then(resp => {
+            if (resp && resp.status === 200) {
+              this.dialogFormVisible = false;
+              this.$emit('onSubmit')
+            }
+          })
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
     }
+  },
+  mounted() {
+    this.getRooms();
   }
+}
 </script>
 
 <style scoped>
-  .add-button {
-    margin: 18px 0 0 10px;
-  }
+.add-button {
+  margin: 18px 0 0 10px;
+}
 </style>
 
 
--- src/components/admin/content/MeetingManagement.vue
+++ src/components/admin/content/MeetingManagement.vue
@@ -7,107 +7,107 @@
         <el-breadcrumb-item>活动管理</el-breadcrumb-item>
       </el-breadcrumb>
     </el-row>
-      <edit-form @onSubmit="loadMeeting()" ref="edit"></edit-form>
+    <edit-form @onSubmit="loadMeeting()" ref="edit"></edit-form>
     <el-card style="margin: 18px 2%;width: 95%">
       <el-table
-              :data="meetings"
-              stripe
-              style="width: 100%"
-              :max-height="tableHeight"
-              :default-sort="{prop: 'startTime', order: 'descending'}">
+          :data="meetings"
+          stripe
+          style="width: 100%"
+          :max-height="tableHeight"
+          :default-sort="{prop: 'startTime', order: 'descending'}">
         <el-table-column
-                type="selection"
-                width="55">
+            type="selection"
+            width="55">
         </el-table-column>
-          <el-table-column type="expand">
-              <template slot-scope="props">
-                  <el-form label-position="left" inline>
-                      <el-form-item>
-                          <span>{{ props.row.abs }}</span>
-                      </el-form-item>
-                  </el-form>
-              </template>
-          </el-table-column>
-        <el-table-column
-                label="开始时间"
-                sortable
-                width="auto">
-            <template slot-scope="scope">
-                <i class="el-icon-time"></i>
-                <span style="margin-left: 10px">{{ scope.row.startTime }}</span>
-            </template>
+        <el-table-column type="expand">
+          <template slot-scope="props">
+            <el-form label-position="left" inline>
+              <el-form-item>
+                <span>{{ props.row.abs }}</span>
+              </el-form-item>
+            </el-form>
+          </template>
         </el-table-column>
         <el-table-column
-                label="结束时间"
-                sortable
-                width="auto">
-            <template slot-scope="scope">
-                <i class="el-icon-time"></i>
-                <span style="margin-left: 10px">{{ scope.row.endTime }}</span>
-            </template>
+            label="开始时间"
+            sortable
+            width="auto">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span style="margin-left: 10px">{{ scope.row.startTime }}</span>
+          </template>
         </el-table-column>
         <el-table-column
-                prop="meetingName"
-                label="会议名称"
-                sortable
-                width="auto">
+            label="结束时间"
+            sortable
+            width="auto">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span style="margin-left: 10px">{{ scope.row.endTime }}</span>
+          </template>
         </el-table-column>
-          <el-table-column
-                  prop="roomName"
-                  label="会议室">
-              <template slot-scope="scope">
-                  <div slot="reference" class="name-wrapper">
-                      <el-tag size="medium">{{ scope.row.roomName }}</el-tag>
-                  </div>
-              </template>
-          </el-table-column>
-          <el-table-column
-                  prop="numberOfParticipants"
-                  label="活动人数">
-          </el-table-column>
-          <el-table-column
-                  prop="description"
-                  label="描述">
-          </el-table-column>
-          <el-table-column
-                  prop="empName"
-                  label="预定用户">
-          </el-table-column>
-          <el-table-column
-                  prop="roomName"
-                  label="预定教室">
-          </el-table-column>
-          <el-table-column
-                  prop="canceledTime"
-                  label="取消时间">
-          </el-table-column>
-          <el-table-column
-                  prop="canceledReason"
-                  label="取消原因">
-          </el-table-column>
-          <el-table-column
-                  label="预定时间">
-              <template slot-scope="scope">
-                  <i class="el-icon-time"></i>
-                  <span style="margin-left: 10px">{{ scope.row.reservationTime }}</span>
-              </template>
-          </el-table-column>
         <el-table-column
-                fixed="right"
-                label="操作"
-                width="120">
-<!--          这里修改了slot-scope -->
+            prop="meetingName"
+            label="会议名称"
+            sortable
+            width="auto">
+        </el-table-column>
+        <el-table-column
+            prop="roomName"
+            label="会议室">
+          <template slot-scope="scope">
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">{{ scope.row.roomName }}</el-tag>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column
+            prop="numberOfParticipants"
+            label="活动人数">
+        </el-table-column>
+        <el-table-column
+            prop="description"
+            label="描述">
+        </el-table-column>
+        <el-table-column
+            prop="empName"
+            label="预定用户">
+        </el-table-column>
+        <el-table-column
+            prop="roomName"
+            label="预定教室">
+        </el-table-column>
+        <el-table-column
+            prop="canceledTime"
+            label="取消时间">
+        </el-table-column>
+        <el-table-column
+            prop="canceledReason"
+            label="取消原因">
+        </el-table-column>
+        <el-table-column
+            label="预定时间">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span style="margin-left: 10px">{{ scope.row.reservationTime }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+            fixed="right"
+            label="操作"
+            width="120">
+          <!--          这里修改了slot-scope -->
           <template slot-scope="scope">
             <el-button
-                    @click.native.prevent="editMeeting(scope.row)"
-                    type="text"
-                    size="small">
+                @click.native.prevent="editMeeting(scope.row)"
+                type="text"
+                size="small">
               编辑
             </el-button>
             <el-button
-                    @click.native.prevent="deleteMeeting(scope.row.meetingId)"
-                    type="text"
-                    size="small">
+                @click.native.prevent="deleteMeeting(scope.row.meetingId)"
+                type="text"
+                size="small">
               移除
             </el-button>
           </template>
@@ -122,78 +122,80 @@
 </template>
 
 <script>
-    import EditForm from "./EditFormMeeting";
-  export default {
-    name: 'MeetingManagement',
-      components: {EditForm},
+import EditForm from "./EditFormMeeting";
 
-      data () {
-          return {
-              meetings: [],
+export default {
+  name: 'MeetingManagement',
+  components: {EditForm},
 
-          }
-      },
-      mounted: function () {
-          this.loadMeeting()
-      },
-      methods: {
-          loadMeeting() {
-              const _this = this;
-              this.$axios.get('/meeting').then(resp => {
-                  if (resp && resp.status === 200) {
-                      _this.meetings = resp.data.result
-                  }
-              })
-          },
-          deleteMeeting (meetingId) {
-              console.log(meetingId)
-              this.$confirm('此操作将永久删除该会议, 是否继续?', '提示', {
-                  confirmButtonText: '确定',
-                  cancelButtonText: '取消',
-                  type: 'warning'
-              }).then(() => {
-                      this.$axios
-                          .post('/meeting/delete', {meetingId: meetingId}).then(resp => {
-                          if (resp && resp.status === 200) {
-                              this.loadMeeting()
-                          }
-                      })
-                  }
-              ).catch(() => {
-                  this.$message({
-                      type: 'info',
-                      message: '已取消删除'
-                  })
-              })
-              // alert(id)
-          },
-          editMeeting (item) {
-              this.$refs.edit.dialogFormVisible = true;
-              this.$refs.edit.form = {
-                  meetingId: item.meetingId,
-                  meetingName: item.meetingName,
-                  reservationIsTid: item.reservationIsTid,
-                  numberOfParticipants: item.numberOfParticipants,
-                  startTime: item.startTime,
-                  endTime: item.endTime,
-                  reservationTime: item.reservationTime,
-                  canceledTime:item.canceledTime,
-                  description:item.description,
-                  status:item.status,
-                  roodId:item.roodId,
-                  roomName:item.roomName,
-                  empName:item.empName,
-                  canceledReason:item.canceledReason
+  data() {
+    return {
+      meetings: [],
 
+    }
+  },
+  mounted: function () {
+    this.loadMeeting();
+  },
+  methods: {
+    loadMeeting() {
+      const _this = this;
+      this.$axios.get('/meeting').then(resp => {
+        if (resp && resp.status === 200) {
+          _this.meetings = resp.data.result
+        }
+      })
+    },
+    deleteMeeting(meetingId) {
+      console.log(meetingId)
+      this.$confirm('此操作将永久删除该会议, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+            this.$axios
+                .post('/meeting/delete', {meetingId: meetingId}).then(resp => {
+              if (resp && resp.status === 200) {
+                this.loadMeeting()
               }
-          },
-      },
-      computed: {
-          tableHeight () {
-              return window.innerHeight - 320
+            })
           }
-      },
-  }
+      ).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+      // alert(id)
+    },
+    editMeeting(item) {
+      console.log(item)
+      this.$refs.edit.dialogFormVisible = true;
+      this.$refs.edit.form = {
+        meetingId: item.meetingId,
+        meetingName: item.meetingName,
+        reservationIsTid: item.reservationIsTid,
+        numberOfParticipants: item.numberOfParticipants,
+        startTime: item.startTime,
+        endTime: item.endTime,
+        reservationTime: item.reservationTime,
+        canceledTime: item.canceledTime,
+        description: item.description,
+        status: item.status,
+        roomId: item.roomId,
+        roomName: item.roomName,
+        empName: item.empName,
+        canceledReason: item.canceledReason
+
+      }
+    },
+  },
+  computed: {
+    tableHeight() {
+      return window.innerHeight - 320
+    }
+  },
+}
 </script>
 
 <style scoped>
